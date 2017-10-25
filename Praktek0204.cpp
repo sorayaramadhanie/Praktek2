3fee1105cb97b6cef55abf38733bc2889e1bebbb
sorayaramadhanie <32513492+sorayaramadhanie@users.noreply.github.com>
2017-10-25 11:25:35 +0800

    Jumlah box parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -8,4 +8,6 @@ int main(){
 	cout<<"Luas Rumah				: "<<LR<<" m2"<<endl;
 	double LP=1.87596;
 	cout<<"Luas 1 box parket		: "<<LP<<" m2"<<endl;
+	double JP=LR/LP;
+	cout<<"Jumlah box parket		: "<<JP<<endl;
 }
